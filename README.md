10df84ebf0ba8cbe526c52134ed3f10d267ab879
Devin <guozwhero@126.com>
2023-09-06 01:59:58 +0800

    666
    666

--- README.md
+++ README.md
@@ -99,3 +99,110 @@ On Windows, if you're seeing an error containing `EPERM` it may be related to sp
   [NEAR Wallet]: https://wallet.testnet.near.org/
   [near-cli]: https://github.com/near/near-cli
   [gh-pages]: https://github.com/tschaub/gh-pages
+
+
+
+===================================================================================================================
+
+
+# 去中心化募捐平台
+## 背景
+针对社会上急需帮助的群体性事件，如：涿州水灾，流浪动物救助，留守儿童失学 等问题，又因为xxx等募捐机构不公开账目，存在侵吞募捐款的可能，现基于区块链去中心化的能力，真正做到公开透明的原则搭建此去中心化募捐平台，发布募捐活动，募集NEAR，为救助对象提供帮助；
+## 平台主要能力
+```shell
+1、发布募捐活动，募集near币
+2、用户参与募捐活动，向募捐指定账户转入NEAR
+3、展示募捐活动详情：参与募捐的人和当前募捐到的NEAR
+```
+
+## 代码仓库
+合约代码：https://github.com/guozhouwei/near_crowdfunding_site/blob/main/near_crowdfunding/src/lib.rs  
+near-api-js: https://github.com/guozhouwei/near_crowdfunding_site/blob/main/src/main.ts
+
+## 合约
+### 业务逻辑图
+![avatar](https://github.com/guozhouwei/near_crowdfunding/blob/master/images/%E5%86%B3%E7%AD%96%E6%B5%81%E7%A8%8B%E5%9B%BE.png)
+
+### 部署和交互
+#### 账户
+假设你注册了4个测试网账户:
+```shell
+1. 主账户（同合约部署签名账户）owner123.testnet
+2. 合约账户 contract1234501.testnet
+3. 募捐人账户 zhouzhou_near.testnet
+```
+
+##### 以上账户私钥保存在 legacy keychain 中
+```shell
+near account import-account using-seed-phrase "${YOUR_SEED_PHRASE}" --seed-phrase-hd-path 'm/44'\''/397'\''/0'\''' network-config testnet
+
+```
+
+#### 编译募捐合约
+1. 进入项目目录
+    ```shell
+   cd .
+    ```
+2. 安装 WASM 工具链
+    ```shell
+   rustup target add wasm32-unknown-unknown
+    ```
+3. 编译合约
+    ```shell
+   RUSTFLAGS="-C link-arg=-s" cargo build --target wasm32-unknown-unknown --release
+    ```
+4. 将合约 WASM 文件移动到项目根目录下方便后续操作
+    ```shell
+   mkdir -p ./res && cp ./target/wasm32-unknown-unknown/release/contract.wasm ./res/
+    ```
+以上操作已经封装在 makefile 文件中
+    ```shell
+   make all
+    ```
+
+#### 合约交互
+1. 部署并初始化合约  
+    ➔ 合约账户：contract1234501.testnet  
+    ➔ near contract deploy contract1234501.testnet use-file ./res/contract.wasm with-init-call init json-args '{"owner_id":"owner123.testnet"}' prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/FFAB7bzLQsyyGbUn5XMZTLWN4rvbJA1npXHjuuzPmj6H  
+
+    1.1 重新部署合约，覆盖原合约  
+    ➔ 合约账户：contract1234501.testnet  
+    ➔ near contract deploy contract1234501.testnet use-file ./res/contract.wasm without-init-call init json-args '{"owner_id":"owner123.testnet"}' prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/HQTcvycQ8CXd8YA5WtQ5FzNCvQbRan4ENaGGqdsnwaAK 
+
+2. 调用 newCampaign 方法，创建募捐活动  
+    ➔ 募捐活动接收账户：owner123.testnet  
+    ➔ near contract call-function as-transaction contract1234501.testnet newCampaign json-args '{"theme":"涿州水灾募捐活动","receiver":"owner123.testnet","number_funders":0,"funding_goal":100}' prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as owner123.testnet network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/A2ZVJi8ihQrAtLY9pQUFFALdJWwC2LDbFBJnbv9xoXv4  
+    
+3. 调用 get_crowdFunding_by_num_campagins 方法，查看创建的募捐活动  
+    ➔ 募捐活动编号：1  
+    ➔ near contract call-function as-transaction contract1234501.testnet get_crowdFunding_by_num_campagins json-args '{"num_campagins":1}' prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as owner123.testnet network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/BA9Rj1EM1Q2fLMmFpbagJd8YiTUCa7KbMb8D11ujEr8H  
+    
+4. 调用 bid 方法, 参与募捐活动  
+    ➔ 参与募捐活动账户：zhouzhou_near.testnet  
+    ➔ 发起转账 from： zhouzhou_near.testnet to ：contract1234501.testnet ，NEAR 88, 
+    near contract call-function as-transaction contract1234501.testnet bid json-args '{"num_campagins":1}' prepaid-gas '100.000 TeraGas' attached-deposit '88 NEAR' sign-as zhouzhou_near.testnet network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/F11L4erVatpe1JnKUk3GaXMrxN1aJEUvXKh3KaEx2h4g  
+    
+5. 调用 get_funders_by_num_campagins 方法, 查看活动募捐人列表  
+    ➔ near contract call-function as-transaction contract1234501.testnet get_funders_by_num_campagins json-args '{"num_campagins":1}' prepaid-gas '100.000 TeraGas' attached-deposit '0 NEAR' sign-as owner123.testnet network-config testnet sign-with-keychain send  
+    ➔ 合约浏览器：https://explorer.testnet.near.org/transactions/7rcGezoSa1cxqB7uMzofok9iynxeJ1K2ThLCw7VoG2fh  
+    
+##### 合约所有交易情况列表：  
+    ➔ https://testnet.nearblocks.io/zh-cn/address/contract1234501.testnet  
+        
+
+## near-api-js
+### 执行命令
+```shell
+yarn start
+```
+### 日志log  
+▶▶▶▶ 募捐活动编号:40  
+▶▶▶▶ 创建募捐活动:'募捐活动编号:40,募捐活动名称:流浪动物救助募捐活动, 募捐收款账号:owner123.testnet, 目标募捐金额:500 NEAR, 参与募捐人数:0, 实际募捐金额:0 yocto (单位：1NEAR = 10^24yoctoNEAR，1NEAR = 10^12Tera)'  
+▶▶▶▶ '第1位募捐人'  
+▶▶▶▶ 捐赠后募捐活动:'募捐活动编号:40,募捐活动名称:流浪动物救助募捐活动, 募捐收款账号:owner123.testnet, 目标募捐金额:500 NEAR, 参与募捐人数:1, 实际募捐金额:8e+24 yocto (单位：1NEAR = 10^24yoctoNEAR，1NEAR = 10^12Tera)'  
+▶▶▶▶ 募捐活动编码:40, 捐赠人:[ { addr: 'zhouzhou_near.testnet', amount: 8e+24 }, [length]: 1   
---
+++ src/App.js
@@ -0,0 +1,288 @@
+import "./App.css";
+import React from 'react';
+// import BN from 'bn.js';
+import * as nearAPI from 'near-api-js'
+import Big from 'big.js';
+
+interface CrowdFunding {
+  num_campagins: number;  //活动编号
+  theme: string;
+  receiver: string;
+  funding_goal: number;
+  number_funders: number;
+  total_amount: number;
+} 
+
+interface Funder {
+  num_campagins: number;
+  addr: string;
+  amount: number;
+} 
+
+const TGas = Big(10).pow(12);
+const BoatOfGas = Big(200).mul(TGas);
+
+const ContractName = 'contract1234501.testnet';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    /**
+     * 上下文信息（合约信息，页面信息等）
+     */
+    this.state = {
+      connected: false, //
+      signedIn: false,  //是否已经登陆钱包
+      accountId: null,  //募捐活动合约授权账户（只有此账户才有权创建募捐活动）
+
+      //contract_accountId: "contract1234501.testnet",  //合约账户（合约已经部署到NEAR测试链上，合约授权账户为：owner123.testnet）
+      contract_sign_accountId: "owner123.testnet",  //合约授权账户
+
+      //创建募捐合同
+      new_crowdfunding_theme:"",
+      new_crowdfunding_receiver:"",
+      new_crowdfunding_funding_goal:0, 
+
+      //募捐活动信息
+      crowdFunding_campagins: new Array(),
+      crowdFunding_funding_map : new Map(), //key：活动编号；value：参与人列表
+      crowdFunding_fundings : new Array(), 
+
+      //
+      crowdfunding_num:0,
+      crowdfunding_goal:0,
+    };
+
+    this.handleNewCrowdChange = this.handleNewCrowdChange.bind(this);
+    this.handleNewReceiverChange = this.handleNewReceiverChange.bind(this);
+    this.handleNewFundingGoalChange = this.handleNewFundingGoalChange.bind(this);
+    this.createNewCrowdSubmit = this.createNewCrowdSubmit.bind(this);
+    this.sendCrowdFundGoalSubmit = this.sendCrowdFundGoalSubmit.bind(this);
+
+
+    this._initNear().then(() => {
+      this.setState({
+        connected: true,
+        signedIn: !!this._accountId,
+        accountId: this._accountId,
+      });
+    }); 
+  }
+
+  handleNewCrowdChange(event) {
+    console.log("募捐活动名称：" + event.target.value);
+    this.setState({new_crowdfunding_theme: event.target.value});
+  }
+
+  handleNewReceiverChange(event) {
+    console.log("募捐活动接收账户：" + event.target.value);
+    this.setState({new_crowdfunding_receiver: event.target.value});
+  }
+
+  handleNewFundingGoalChange(event) {
+    console.log("募捐活动筹集目标金额：" + event.target.value);
+    this.setState({new_crowdfunding_funding_goal: event.target.value});
+  }
+
+  handlewCrowdFundingNumChange(event) {
+    console.log("参与募捐活动的编号：" + event.target.value);
+    this.setState({crowdfunding_num: event.target.value});
+  }
+
+  handlewCrowdFundingGoalChange(event) {
+    console.log("参与募捐的金额：" + event.target.value);
+    this.setState({crowdfunding_goal: event.target.value});
+  }
+
+  createNewCrowdSubmit(event) {
+    this.createNewCrowd();
+    event.preventDefault();
+  }
+
+  async createNewCrowd() {
+    let num_campagins = await this._contract.newCampaign({"theme":this.state.new_crowdfunding_theme, "receiver":this.state.new_crowdfunding_receiver, "number_funders":0, "funding_goal":Number(this.state.new_crowdfunding_funding_goal)});
+    if(num_campagins > 0 ) {
+      alert("合约创建成功，合约编号：" + num_campagins);
+      if (this._accountId) {
+        await this.refreshAccountStats();
+      }
+    }
+  }
+
+  sendCrowdFundGoalSubmit(event) {
+    this.bidCrowdFund();
+    event.preventDefault();
+  }
+
+  async bidCrowdFund() {
+    let funder_number = await this._contract.bid({"num_campagins":Number(this.state.crowdfunding_num)});
+    if(funder_number > 0 ) {
+      console.log("第：" + funder_number + "位捐赠人");
+      if (this._accountId) {
+        await this.refreshAccountStats();
+      }
+    }
+  }
+
+  async _initNear() {
+    const nearConfig = {
+      networkId: 'default',
+      nodeUrl: 'https://rpc.testnet.near.org',
+      contractName: ContractName,
+      walletUrl: 'https://wallet.testnet.near.org',
+    };
+    const keyStore = new nearAPI.keyStores.BrowserLocalStorageKeyStore();
+    const near = await nearAPI.connect(Object.assign({ deps: { keyStore } }, nearConfig));
+    this._keyStore = keyStore;
+    this._nearConfig = nearConfig;
+    this._near = near;
+
+    this._walletConnection = new nearAPI.WalletConnection(near, ContractName);
+    this._accountId = this._walletConnection.getAccountId();
+    this._account = this._walletConnection.account();
+    //
+    console.log("_accountId=" + this._accountId + ", _account=" + this._account);
+    //
+    
+    this._contract = new nearAPI.Contract(this._account, ContractName, {
+      viewMethods: ['get_crowdFunding_by_num_campagins', 'get_all_crowdFunding', 'get_funders_by_num_campagins'],
+      changeMethods: ['newCampaign', 'bid'],
+    });
+
+    if (this._accountId) {
+      await this.refreshAccountStats();
+    }
+  }
+
+  async refreshAccountStats() {
+    let crowdFunding_campagin_arry:CrowdFunding[]|undefined|null = await this._contract.get_all_crowdFunding();
+    for(var i=0; i<crowdFunding_campagin_arry.length; i++){
+      let crowdFunding_campagin_item = crowdFunding_campagin_arry[i];
+      crowdFunding_campagin_item.num_campagins = i + 1;
+      this.state.crowdFunding_campagins.push(crowdFunding_campagin_item);
+      //
+      let crowdFunding_funding_arry:Funder[]|undefined|null = await this._contract.get_funders_by_num_campagins({num_campagins: crowdFunding_campagin_item.num_campagins});
+      if(crowdFunding_funding_arry.length > 0 ) {
+        this.state.crowdFunding_funding_map.set(crowdFunding_campagin_item.num_campagins, crowdFunding_funding_arry);
+      }
+    }
+  }
+
+  async requestSignIn() {
+    const appTitle = '去中心化募捐平台';
+    await this._walletConnection.requestSignIn(
+      ContractName,
+      appTitle
+    )
+  }
+
+  async logOut() {
+    this._walletConnection.signOut();
+    this._accountId = null;
+    this.setState({
+      signedIn: !!this._accountId,
+      accountId: this._accountId,
+    })
+  }
+
+  render() {
+    console.log("call render().");
+    const content = !this.state.connected ? (
+      <div>
+        正在链接NEAR钱包 ...
+        <span className="spinner-grow spinner-grow-sm" role="status" aria-hidden="true"></span>
+      </div>
+    ) : (this.state.signedIn ? (
+      <div>
+        <div className="float-right">
+          <button className="btn btn-outline-secondary" onClick={() => this.logOut()}>退出</button>
+        </div>
+        <h5><span className="font-weight-bold">当前登陆NEAR账号：{this.state.accountId}</span></h5>
+        <div>
+      </div>
+      {
+        this.state.contract_sign_accountId === this.state.accountId ? 
+        <div>
+          <h6><span className="font-weight-bold">您有权创建募捐活动</span></h6>
+          <hr></hr>
+          <label style={{marginTop:"10px"}}> 〓 创建募捐活动〓 </label>
+          <form onSubmit={this.createNewCrowdSubmit}>
+            <label>活动名称： <input type="text" style={{marginLeft:"10px"}} value={this.state.new_crowdfunding_theme} onChange={(event) => {this.handleNewCrowdChange(event)}} /> </label><br/>
+            <label>接收募捐账户：<input type="text" style={{marginLeft:"10px"}} value={this.state.new_crowdfunding_receiver} onChange={(event) => {this.handleNewReceiverChange(event)}} /> </label><br/>
+            <label>募捐目标金额： <input type="number" style={{marginLeft:"10px"}} value={this.state.new_crowdfunding_funding_goal} onChange={(event) => {this.handleNewFundingGoalChange(event)}} /> </label><br/>
+            <input type="submit" value="提交" />
+          </form>
+
+          <hr></hr>
+          <div style={{fontSize: '16px'}}>
+          <label> 〓 已创建的募捐活动列表 〓 </label> <br/>
+            {
+                this.state.crowdFunding_campagins.map(item=>(
+                    <li key={item} >『编号』：{item.num_campagins +", "} 『活动名称』：{item.theme +", "} 『募捐接收账户』：{item.receiver + ", " } 『募资目标金额』：{item.funding_goal + "NEAR, " } 『募资参与人数』：{item.number_funders + ", "}  『已经募集的金额』：{item.total_amount + " yoctoNEAR"} 
+                      <br/>
+                      <label style={{fontSize: '13px'}}>  ✿ 募捐人列表 </label> <br/>
+                      <div style={{fontSize: '10px'}}>
+                        {
+                          this.state.crowdFunding_funding_map.get(item.num_campagins) != null && this.state.crowdFunding_funding_map.get(item.num_campagins) != undefined && this.state.crowdFunding_funding_map.get(item.num_campagins).length > 0 ? (
+                            this.state.crowdFunding_funding_map.get(item.num_campagins).map(item12=>(
+                              <div key={item12}> ◦  捐赠人地址：{item12.addr +", "} 捐赠金额：{item12.amount + " yoctoNEAR"} <br/></div>
+                            )  
+                          )
+                          ):(
+                              <label> 无捐赠人.</label> 
+                          )
+                        }
+                      </div>
+                    </li>
+                  ))
+            }
+          </div>
+          <br/>
+          
+          <hr></hr>
+        </div>
+        
+       : 
+       
+        <div>
+          <h6><span className="font-weight-bold">欢迎您参与募捐活动</span></h6>
+          <hr></hr>
+          <label> 请选择您要参与的募捐活动 </label> <br/>
+          {
+            (
+            this.state.crowdFunding_campagins.map(item=>(
+              <label>
+                『编号』: {item.num_campagins +", "} 『活动名称』: {item.theme +", "} 『募捐接收账户』: {item.receiver + ", " } 『募捐人数』: {item.number_funders}
+                <br/>
+              </label>
+            ))
+            )
+          }
+          <hr></hr>
+          <form onSubmit={this.sendCrowdFundGoalSubmit}>
+            <label>活动编号： <input type="text" style={{marginLeft:"10px"}} value={this.state.crowdfunding_num} onChange={(event) => {this.handlewCrowdFundingNumChange(event)}} /> </label><br/>
+            <label>募捐金额： <input type="number" style={{marginLeft:"10px"}} value={this.state.crowdfunding_goal} onChange={(event) => {this.handlewCrowdFundingGoalChange(event)}} /> </label><br/>
+            <input type="submit" value="提交" />
+          </form>
+        </div>
+      }
+      </div>
+    ) : (
+      <div style={{ marginBottom: "10px" }}>
+        <button
+          className="btn btn-primary"
+          onClick={() => this.requestSignIn()}>登陆NEAR钱包</button>
+      </div>
+    ));
+    return (
+        <div className="px-5">
+          <h1>NEAR去中心化募捐活动平台</h1>
+          <span className="font-weight-light">(提示⚠️：募捐活动合约已经部署到测试链，合约账户：{ContractName}，授权账户：{this.state.contract_sign_accountId})</span>
+          {content}
+        </div>
+    );
+  }
+}
+
+export default App;
